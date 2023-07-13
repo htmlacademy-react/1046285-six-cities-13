92edca821825af7596ef04563f19b376bbda4ab2
chugaev101 <chugaev101@gmail.com>
2023-07-13 08:35:57 +0300

    Implements routing and creates a private route component.

---
+++ src/components/private-route/private-route.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from "react";
+import { Navigate } from "react-router-dom";
+import { AppRoute, AuthorizationStatus } from "../../const";
+
+type PrivateRouteProps = PropsWithChildren<{
+  authorizationStatus: AuthorizationStatus;
+}>;
+
+const PrivateRoute = ({authorizationStatus, children}: PrivateRouteProps) => {
+  return authorizationStatus === AuthorizationStatus.Auth ? (
+    children
+  ) : (
+    <Navigate to={AppRoute.Login} />
+  );
+};
+
+export { PrivateRoute };
